f2e4abf5a6efd4f911c2bf2a0a901390d332f656
Lesi <alesiastewart@Lesis-MacBook-Pro.local>
2020-05-08 17:27:15 -0500

    final_webpage is connected to flask app

---
+++ final_webpage/app.py
@@ -0,0 +1,56 @@
+import numpy as np
+import pickle
+from flask import Flask, redirect,url_for,render_template, request,jsonify
+from flask_bootstrap import Bootstrap
+from subprocess import Popen
+# 2. Create an app, being sure to pass __name__
+app = Flask(__name__, static_folder='./')
+Bootstrap(app)
+model_open = pickle.load(open('model.pkl','rb'))
+ 
+
+# 3. Define what to do when a user hits the index route
+@app.route('/index.html')
+@app.route('/')
+def home():
+   return app.send_static_file("index.html")
+
+@app.route('/data.html')
+def data():
+    return app.send_static_file("data.html")
+
+@app.route('/model.html')
+def model_player():
+    return app.send_static_file("model.html")
+
+@app.route('/model_position.html')
+def model_position():
+   return app.send_static_file("model_position.html")
+
+@app.route('/player_shots.html')
+def player_shots():
+   return app.send_static_file("player_shots.html")
+ 
+@app.route('/position_shots.html')
+def position_shots():
+   return app.send_static_file("position_shots.html")
+
+  
+#@app.route('/model_position.html',methods = ['POST'])
+##def model():
+ ## int_features = [int(x) for x in request.form.values()]
+   ## final_features = [np.array(int_features)]
+    ##prediction = model.predict(final_features)
+
+    ##output = round(prediction[0],2)
+    ##return render_template("model_position.html", prediction_text = 'Shot was made {}').format(output)
+
+@app.route('/predict', methods=['GET'])
+def predict():
+    return jsonify("test")
+ 
+if __name__ == "__main__":
+    app.run(host='localhost', port=5000)
+app.run(debug=True)
+
+
--- final_webpage/js/model.js
+++ final_webpage/js/model.js
@@ -1,4 +1,4 @@
-var bounds = [[0,0], [940,500]];
+var bounds = [[0, 0], [940, 500]];
 
 var map = L.map('leftCourt', {
     crs: L.CRS.Simple,
@@ -16,17 +16,33 @@ map.fitBounds(bounds);
 
 var theMarker = {};
 
-let shooter = "Point Guard";
-
+var data = [];
+let shooter = d3.select("#probability");
+let dropdown = d3.select("#target_names");
 let probability = "0.57";
 
 
-map.on('click',function(e){
+shooter.on("click", function () {
+    console.log(dropdown.text())
+    var currentSelect = dropdown.node().selectedOptions[0].label
+    data.push(["upvote", currentSelect]);
+    console.log(data)
+    d3.select('#outputposition').text(currentSelect);
+    //d3.select('#outputprob').text(probability);
+    d3.json("http://localhost:5000/predict").then(function (json) {
+        console.log(json);
+        d3.select('#outputprob').text(json.toString());
+    });
+
+
+});
+
+map.on('click', function (e) {
     posLat = parseInt(e.latlng.lat);
     posLon = parseInt(e.latlng.lng);
 
-    console.log("You clicked the map at LAT: "+ posLat+" and LONG: "+posLon );
-      //Clear existing marker, 
+    console.log("You clicked the map at LAT: " + posLat + " and LONG: " + posLon);
+    //Clear existing marker, 
 
     if (theMarker != undefined) {
         map.removeLayer(theMarker);
@@ -34,16 +50,15 @@ map.on('click',function(e){
 
     emptyTheBoxes();
 
-  //Add a marker to show where you clicked.
-    theMarker = L.marker([posLat,posLon]).addTo(map);
+    //Add a marker to show where you clicked.
+    theMarker = L.marker([posLat, posLon]).addTo(map);
 
     fillTheBoxes();
 });
 
 
-
 function emptyTheBoxes() {
-    let shooterInformation = d3.select("#positionShotResponse");
+    let shooterInformation = shooter;
     let probabilityInformation = d3.select("#positionProbabilityResponse")
 
     shooterInformation.html("");
@@ -54,7 +69,7 @@ function fillTheBoxes() {
     let shooterInformation = d3.select("#positionShotResponse");
     let probabilityInformation = d3.select("#positionProbabilityResponse")
 
-    shooterInformation.append("h5").text(shooter);
+    shooterInformation.append("h5").text(data);
     probabilityInformation.append("h5").text(probability);
 
 }
